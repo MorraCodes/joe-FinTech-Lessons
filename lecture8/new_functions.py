ffa2f15147897a5f8fce0634315ce96f9ec975e3
MorraCodes <omarmoh.said@yahoo.com>
2021-08-31 21:00:46 -0400

    Add files via upload

---
+++ lecture8/new_functions.py
@@ -0,0 +1,17 @@
+import statistics
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+def mul(num1, num2):
+    return num1 * num2
+
+
+def mean(numbers):
+    return statistics.mean(numbers)
